35e8d5390469277adf06353119afe6a88921c519
Junaid Ahamed <junuhammad2@gmail.com>
2026-04-14 17:27:30 +0530

    made matrixrain a lighter shade

--- src/App.js
+++ src/App.js
@@ -331,7 +331,7 @@ function startMatrix(canvasId) {
     context.fillStyle = "rgba(0, 0, 0, 0.05)";
     context.fillRect(0, 0, canvas.width, canvas.height);
 
-    context.fillStyle = "rgb(49, 105, 24)";
+    context.fillStyle = "rgb(83, 169, 43)";
     context.font = fontSize + "px monospace";
 
     for (let i = 0; i < rainDrops.length; i++) {
